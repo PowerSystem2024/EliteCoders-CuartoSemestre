ea8abb1b1aaad081bdb4a462aca12f772e1f1f2c
Matias Garcia <95774271+matiasgarcia-dot@users.noreply.github.com>
2025-11-09 21:09:44 -0300

    Clase 12 Python en Entorno Profesional -> Parte 7

--- Python/py-projects/python-pip/web-server/main.py
+++ Python/py-projects/python-pip/web-server/main.py
@@ -1,8 +1,27 @@
 import store
+from fastapi import FastApi
+from fastapi.responses import HTMLResponse
+
+app= FastApi()# Creamos nuestra primera instacia
+
+@app.get('/') # Agregamos decorador de ruta
+def get_list():
+    return[1,2,3]
+
+#Segunda ruta
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return'''
+        <h1> Soy un sitio web </h1>
+        <p>Soy un parrafo
+
+'''
+
 
 
 def run():
     store.get_razas()
+    
 
 if __name__=="__main__":
     run()
